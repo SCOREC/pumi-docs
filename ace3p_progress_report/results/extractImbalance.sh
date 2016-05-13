06ab671cc424fd5dca3d133a8a191f5d7535f585
Cameron Smith <smithc11@rpi.edu>
2016-05-13 17:00:59 -0400

    add ghost ents

--- ace3p_progress_report/results/extractImbalance.sh
+++ ace3p_progress_report/results/extractImbalance.sh
@@ -2,7 +2,11 @@ echo tot vtx
 grep 'Total Vertices Max' slurm-2189328.imb.out | awk '{print $5}'
 echo imb vtx
 grep 'Total Vertices Max' slurm-2189328.imb.out | awk '{print $(NF)}'
+echo ghost vtx
+grep 'Ghost Vertices Max' slurm-2189328.imb.out | awk '{print $(5)}'
 echo tot elm
 grep 'Total Elements Max' slurm-2189328.imb.out | awk '{print $5}'
 echo imb elm
 grep 'Total Elements Max' slurm-2189328.imb.out | awk '{print $(NF)}'
+echo ghost elm
+grep 'Ghost Elements Max' slurm-2189328.imb.out | awk '{print $(5)}'
