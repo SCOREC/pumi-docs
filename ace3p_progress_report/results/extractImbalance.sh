659850e38591bb8063353a256c57fc4863ce6b4a
Cameron Smith <smithc11@rpi.edu>
2016-05-18 10:57:24 -0400

    log file set on cmd line

--- ace3p_progress_report/results/extractImbalance.sh
+++ ace3p_progress_report/results/extractImbalance.sh
@@ -1,12 +1,14 @@
+#!/bin/bash -e
+log=$1
 echo tot vtx
-grep 'Total Vertices Max' slurm-2189328.imb.out | awk '{print $5}'
+grep 'Total Vertices Max' $log | awk '{print $5}'
 echo imb vtx
-grep 'Total Vertices Max' slurm-2189328.imb.out | awk '{print $(NF)}'
+grep 'Total Vertices Max' $log | awk '{print $(NF)}'
 echo ghost vtx
-grep 'Ghost Vertices Max' slurm-2189328.imb.out | awk '{print $(5)}'
+grep 'Ghost Vertices Max' $log | awk '{print $(5)}'
 echo tot elm
-grep 'Total Elements Max' slurm-2189328.imb.out | awk '{print $5}'
+grep 'Total Elements Max' $log | awk '{print $5}'
 echo imb elm
-grep 'Total Elements Max' slurm-2189328.imb.out | awk '{print $(NF)}'
+grep 'Total Elements Max' $log | awk '{print $(NF)}'
 echo ghost elm
-grep 'Ghost Elements Max' slurm-2189328.imb.out | awk '{print $(5)}'
+grep 'Ghost Elements Max' $log | awk '{print $(5)}'
