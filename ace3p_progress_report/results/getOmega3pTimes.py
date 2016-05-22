e759fd1df15211f384862cfeb4bcff26e2297083
Cameron Smith <smithc11@rpi.edu>
2016-05-22 10:13:40 -0400

    better formatting

--- ace3p_progress_report/results/getOmega3pTimes.py
+++ ace3p_progress_report/results/getOmega3pTimes.py
@@ -3,14 +3,16 @@ import sys
 import re
 
 keys={ \
+'omega3p \d* \d* noreorder': 1, \
+'omega3p \d* \d*$': 1, \
+'omega3p_pumi \d* \d* noreorder': 1, \
+'omega3p_pumi \d* \d*$': 1, \
 'Number of MPI processes:': -1, \
 'Number of compute nodes:': -1, \
 'Number of processes per node:': -1, \
 'Time for setting up finite element framework:':-1, \
 'Analysis step:': -2, \
 'Factorization step:': -2, \
-'used mem per MPI process:.*max:': 9, \
-'used mem per MPI process:.*avg': 12, \
 'Linear Solver Preparation Time:':-1, \
 'Solver Time:': -1, \
 'Omega3p ran in': -2 ,\
@@ -28,11 +30,12 @@ keys={ \
 'Time for save/load ComputationalMesh': -1 \
 }
 
-
-
 def printlog(log):
     for k in sorted(log.keys()):
-        print(k,',',log[k])
+        print(k,end=',')
+        for i in log[k]:
+           print(i,end=',')
+        print()
 
 if len(sys.argv) is not 2:
     print('Usage: ', sys.argv[0], ' <omega3p output file>')
@@ -41,13 +44,27 @@ if len(sys.argv) is not 2:
 print('file',',',sys.argv[1])
 f = open(sys.argv[1],'r')
 
+runs = []
 log = {}
 for line in f:
     for k,v in keys.items():
         m = re.search(k, line)
         if m:
             log[k] = line.split()[v]
-    if line.startswith('ACE3P git repo hash'):
-        printlog(log)
+    if line.startswith('Omega3p peak memory usage'): #last line
+        runs.append(log)
+        log = {}
 f.close()
+
+log = {}
+for k in sorted(keys):
+    log[k] = []
+print(len(runs))
+for run in runs:
+    for k in sorted(keys):
+        if k in run:
+          log[k].append(run[k])
+        else:
+          log[k].append('')
+
 printlog(log)
